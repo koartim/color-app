9b130ccc94a34dc0ce0c11a23f7282a8a5947c2e
Tim Koar <koartimothy@gmail.com>
2020-04-22 17:05:35 -0400

    styled color box

--- src/components/ColorBox.js
+++ src/components/ColorBox.js
@@ -5,9 +5,20 @@ export class ColorBox extends Component {
     render() {
         console.log(this.props);
         
+        const {background, name} =  this.props
+        
         return (
-            <div style={{background: this.props.background}} className="ColorBox">
-                <span>{this.props.name}</span>
+            <div style={{background: background}} className="ColorBox">
+                <div className="copy-container">
+                    <div className="box-content">
+                    <span>{name}</span>
+                    </div>
+                        
+                </div>
+                <button className="copy-button">Copy</button>
+                <div>
+                    <span className="see-more">MORE</span>
+                </div>
             </div>
         );
     }
