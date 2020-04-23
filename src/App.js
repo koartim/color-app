ceb1fe01ccc0031a22bbe8100e21ac59eb3e847e
Tim Koar <koartimothy@gmail.com>
2020-04-23 13:10:43 -0400

    add slider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Palette from './components/Palette';
 import seedColors from './seedColors';
+import { generatePalette } from './colorHelpers';
 
 function App() {
   return (
     <div className="App">
-      <Palette {...seedColors[5]}/>
+      <Palette palette={generatePalette(seedColors[4])}/>
     </div>
   );
 }
